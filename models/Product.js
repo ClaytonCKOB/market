b97b3242442e9b71ac8e9e2acb054e8d83060d4f
ClaytonCKOB <claytoncaco89@gmail.com>
2023-05-27 20:01:37 -0300

    creating product model

---
+++ models/Product.js
@@ -0,0 +1,46 @@
+modules.exports = (sequelize, DataTypes) => {
+    const Product = sequelize.define('product', {
+        cod: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            validate: {
+                notEmpty: true
+            }
+        },
+        description: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                notEmpty: false
+            }
+        },
+        cost: {
+            type: DataTypes.FLOAT,
+            allowNull: false,
+            validate: {
+                notEmpty: false
+            }
+        },
+        price: {
+            type: DataTypes.FLOAT,
+            allowNull: false,
+            validate: {
+                notEmpty: false
+            }
+        },
+        supplier: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            validate: {
+                notEmpty: false
+            }
+        },
+        category: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            validate: {
+                notEmpty: false
+            }
+        },
+    });
+};
