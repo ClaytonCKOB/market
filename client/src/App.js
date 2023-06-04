1b07dcd256b074fb6bb58eb0afde29ca0abbe53d
ClaytonCKOB <claytoncaco89@gmail.com>
2023-06-04 00:35:07 -0300

    base of index page

--- client/src/App.js
+++ client/src/App.js
@@ -4,15 +4,60 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img  className="App-logo" alt="logo" />
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="modal payment_methods">
+          <div className="modal-content">
+              <h2>Forma de pagamento:</h2>
+              <div className="payment_item" payment="1" id="1">
+                  <p>Dinheiro</p>
+              </div>
+              <div className="payment_item" payment="0" id="2">
+                  <p>Pix</p>
+              </div>
+              <div className="payment_item" payment="0" id="3">
+                  <p>Cartão de Crédito</p>
+              </div>
+              <div className="payment_item" payment="0" id="4">
+                  <p>Cartão de Débito</p>
+              </div>
+          </div>
+      </div>
+      <div className="modal resume">
+          <div className="modal-content">
+              <div className="modal_title">
+                  <h2>Resumo:</h2>
+              </div>
+              <div className="resume_content">
+
+              </div>
+          </div>
+      </div>
+      <div id="container">
+          <div className="products_side">
+              <input id="cod_barras" type="text"/>
+              <div id="cart">
+              </div>
+          </div>
+          <div className="info_side">
+              <div id="total">
+                  <p id="sub-total" className="header">SUB-TOTAL:</p>
+                  <div className="card_content">
+                      <p className="info_value total_value" id="price">0,00</p>
+                  </div>
+              </div>
+              <div id="unit_value">
+                  <p className="header" >VALOR UNITÁRIO:</p>
+                  <div className="card_content">
+                      <p className="info_value unit_value">0,00</p>
+                  </div>
+              </div>
+              <div id="item_info">
+                  <p className="header">VALOR TOTAL DO ITEM:</p>
+                  <div className="card_content">
+                      <p className="info_value total_value">0,00</p>
+                  </div>
+              </div>
+          </div>
+        </div>
       </header>
     </div>
   );
--- index.js
+++ index.js
@@ -7,6 +7,10 @@ const productRouter = require('./router/Product');
 
 app.use('/product', productRouter);
 
+app.get('/', (res, req) => {
+    res.render('/client/src/App.js');
+});
+
 
 db.sequelize.sync({force: true}).then((req) => {
     app.listen('3001', (err, res) => {
