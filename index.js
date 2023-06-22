9648374d0161f9928cff6bcda33fe607385aa78c
Gabriel Pereira <gabipereira8282@gmail.com>
2023-06-22 09:25:52 -0300

    adding cors

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const db = require('./models');
 const express = require('express');
+const cors = require('cors');
 const app = express();
 
+
+
 const productRouter = require('./router/Product');
-
-
 app.use('/product', productRouter);
+
+app.use(cors());
 
 app.get('/', (res, req) => {
     res.render('/client/src/App.js');
