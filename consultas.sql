570c00bfd6568a987be4ec89fefe5c5bd3b78135
Gabriel Pereira <gabipereira8282@gmail.com>
2023-08-19 14:18:47 -0300

    updating tables and querries

--- consultas.sql
+++ consultas.sql
@@ -1,59 +1,83 @@
---Consulta de vendas realizadas por cliente, mostrando o total gasto por cada cliente:
-
-SELECT c.nome AS cliente, COUNT(*) AS num_vendas, SUM(v.total) AS total_gasto
-FROM cliente c
-LEFT JOIN venda v ON c.id = v.cliente
+-- Consulta com o nome do clientes cadastrados, quantidade de vendas para cada cliente, mostrando o total gasto por cada cliente:
+SELECT c.nome AS cliente, COUNT(distinct r.nota_fiscal) AS num_vendas, SUM(r.valor_final) AS total_gasto
+FROM clientes c
+LEFT JOIN vendas v ON c.id = v.cliente
+JOIN resumo_carrinhos r ON v.nota_fiscal = r.nota_fiscal
 GROUP BY c.nome;
 
--- Consulta de produtos vendidos em cada categoria, com a quantidade total vendida e o preço médio:
+-- Consulta com todas as categorias, a quantidade total vendida em cada uma e o valor total das vendas:
+SELECT c.nome,COUNT(categoria) as quantidade,SUM(valor_final) as total_vendido
+FROM resumo_carrinhos r
+JOIN produtos p USING (cod_barras)
+RIGHT JOIN categorias c ON c.id = p.categoria
+GROUP BY c.nome;
 
-SELECT cat.nome AS categoria, p.descricao AS produto, SUM(vd.quantidade) AS quantidade_vendida, AVG(p.preco) AS preco_medio
-FROM categoria cat
-JOIN produto p ON cat.id = p.categoria
-LEFT JOIN venda_detalhes vd ON p.descricao = vd.produto
-GROUP BY cat.nome, p.descricao;
+-- Consulta com o nome dos funcionarios, o turno em que ele trabalha e a soma da vendas que ele fez
+SELECT f.nome as funcionario, t.nome as turno, SUM(r.valor_final)
+FROM funcionarios f
+LEFT JOIN turnos t ON f.turno = t.id
+LEFT JOIN vendas v ON v.vendedor = f.id
+LEFT JOIN resumo_carrinhos r USING (nota_fiscal)
+GROUP BY f.nome,t.nome;
 
--- Consulta de clientes que compraram mais de 3 vezes e gastaram mais de R$100 em cada compra:
+-- Consulta de todas as vendas que utilizaram mais de um metodo de pagamento e o valor dela
+Select nota_fiscal, SUM(valor_final) as valor
+FROM resumo_carrinhos r
+JOIN vendas v USING(nota_fiscal)
+JOIN pagamentos p ON (p.venda = v.nota_fiscal)
+GROUP BY nota_fiscal
+HAVING COUNT(distinct metodo_pagamento) > 1;
 
-SELECT c.nome AS cliente, COUNT(*) AS num_compras, SUM(v.total) AS total_gasto
-FROM cliente c
-JOIN venda v ON c.id = v.cliente
-GROUP BY c.nome
-HAVING num_compras > 3 AND MIN(total_gasto) > 100;
+-- Consulta com a categoria  e o nome de todos produtos que não foram vendidos ainda
+SELECT cat.nome as Categoria, p.descricao as Produto 
+FROM produtos p
+JOIN categorias cat ON (cat.id = p.categoria)
+WHERE p.cod_barras NOT IN (	SELECT produto 
+							FROM carrinhos);
+                            
+-- Consulta dos fornecedores que vendem os mesmos produtos, ou mais, que o fornecedor com id = 1 e o nome dos produtos
+SELECT fornecedores.nome,produtos.descricao
+FROM fornecedores
+JOIN fornecimentos ON (id = fornecedor)
+JOIN produtos ON (produto = cod_barras)
+WHERE id <> 1 AND 
+	NOT EXISTS(
+		SELECT produto 
+        FROM fornecimentos 
+        WHERE fornecedor = 1 AND
+        fornecedor NOT IN ( SELECT DISTINCT produto 
+					FROM fornecimentos
+                    WHERE fornecedor = fornecedores.id));
 
--- Consulta dos produtos que não foram vendidos ainda:
-
-SELECT p.descricao AS produto
-FROM produto p
-WHERE NOT EXISTS (SELECT * FROM venda_detalhes vd WHERE p.descricao = vd.produto);
-
--- Consulta dos fornecedores que não possuem nenhum produto cadastrado:
-
-SELECT f.nome AS fornecedor
-FROM fornecedor f
-WHERE NOT EXISTS (SELECT * FROM produto p WHERE f.id = p.fornecedor);
-
--- Consulta das vendas que foram realizadas utilizando o método de pagamento "Dinheiro":
-
-SELECT v.id AS venda_id, c.nome AS cliente, v.total
-FROM venda v
-JOIN cliente c ON v.cliente = c.id
-WHERE v.metodo_pagamento = (SELECT id FROM metodo_pagamento WHERE descricao = 'Dinheiro');
-
--- Consulta de produtos vendidos com descontos acima de 20%:
-
-SELECT p.descricao AS produto, vd.quantidade, d.porcentagem AS desconto
-FROM produto p
-JOIN venda_detalhes_aux vd ON p.descricao = vd.produto
-JOIN venda v ON vd.venda_id = v.id
-JOIN descontos d ON v.desconto = d.id AND d.porcentagem > 20;
-
--- Consulta de clientes que compraram produtos da categoria "Alimentos" e "Bebidas":
-
-SELECT c.nome AS cliente, p.descricao AS produto
-FROM cliente c
-JOIN venda v ON c.id = v.cliente
-JOIN venda_detalhes_aux vd ON v.id = vd.venda_id
-JOIN produto p ON vd.produto = p.descricao
-JOIN categoria cat ON p.categoria = cat.id
-WHERE cat.nome IN ('Alimentos', 'Bebidas');
+-- Consulta que zera os descontos dos produtos fornecidos somente pelo 'Fornecedor B'
+UPDATE produtos
+SET desconto = 0
+WHERE cod_barras IN (
+	SELECT produto
+	FROM fornecimentos
+	JOIN fornecedores ON fornecedores.id = fornecimentos.fornecedor
+    WHERE produto IN ( 
+		SELECT produto 
+        FROM fornecimentos 
+        where fornecedor = (
+			SELECT id 
+            from fornecedores 
+            where nome = 'Fornecedor B'
+            ))
+	GROUP BY produto
+	HAVING COUNT(produto) = 1);
+	
+    
+-- Consulta que mostra os detalhes de todas as vendas entregues a noite
+SELECT ven.nota_fiscal as nota_fiscal, cli.nome as cliente, SUM(res.valor_total_bruto) as total,
+	SUM(res.desconto) as desconto, SUM(res.valor_final) as valor_final, ven.data as data,cli.endereco as endereço,
+    vendedor.nome as vendedor,ent.nome as entregador,vei.modelo as veiculo
+FROM vendas ven
+JOIN resumo_carrinhos res USING(nota_fiscal)
+JOIN clientes cli ON ven.cliente = cli.id
+LEFT JOIN funcionarios ent ON ven.entregador = ent.CNH
+LEFT JOIN funcionarios vendedor ON ven.vendedor = vendedor.id
+LEFT JOIN veiculos vei ON vei.id = ven.veiculo
+LEFT JOIN turnos tur ON tur.id = ent.turno
+WHERE tur.nome = 'Noite'
+GROUP BY ven.nota_fiscal
--- instancias.sql
+++ instancias.sql
@@ -1,142 +1,254 @@
 delimiter \\
-CREATE TABLE IF NOT EXISTS categoria (
-  id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-  nome VARCHAR(40) NOT NULL,
-  PRIMARY KEY (id)
+
+drop database market;
+create database market;
+use market;
+
+CREATE TABLE IF NOT EXISTS categorias (
+	id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+	nome VARCHAR(40) NOT NULL,
+	PRIMARY KEY (id)
 );
 
-CREATE TABLE IF NOT EXISTS metodo_pagamento (
-  id INT NOT NULL AUTO_INCREMENT, 
-  descricao VARCHAR(40),
-  PRIMARY KEY (id)
+CREATE TABLE IF NOT EXISTS fornecedores (
+	id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+	nome VARCHAR(40) NOT NULL,
+	cpf_cnpj VARCHAR(20) NOT NULL UNIQUE,
+	telefone VARCHAR (11) NOT NULL,
+	email VARCHAR(40) NOT NULL,
+	PRIMARY KEY (id)
 );
 
-CREATE TABLE IF NOT EXISTS fornecedor (
-  id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-  nome VARCHAR(40) NOT NULL,
-  telefone INT UNSIGNED NOT NULL,
-  email VARCHAR(40) NOT NULL,
-  PRIMARY KEY (id)
+CREATE TABLE IF NOT EXISTS produtos (
+	cod_barras INT UNSIGNED AUTO_INCREMENT NOT NULL,
+	descricao VARCHAR(50) NOT NULL,
+	custo FLOAT(6, 2) NOT NULL,
+	preco FLOAT(6, 2) NOT NULL,
+	categoria INT UNSIGNED,
+	desconto FLOAT(6,2),
+	PRIMARY KEY (cod_barras),
+	FOREIGN KEY (categoria) 
+		REFERENCES categorias(id)
+		ON DELETE SET NULL
+        ON UPDATE CASCADE,
+	CHECK ((preco > custo) and custo > 0 and preco > 0) 
 );
 
-CREATE TABLE IF NOT EXISTS cliente (
-  id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-  nome VARCHAR(40),
-  email VARCHAR(40),
-  cpf VARCHAR(11) UNIQUE,
-  telefone VARCHAR(11),
-  endereco VARCHAR(30),
-  PRIMARY KEY (id)
+
+
+CREATE TABLE IF NOT EXISTS fornecimentos(
+	produto INT UNSIGNED NOT NULL,
+	fornecedor INT UNSIGNED NOT NULL,
+	PRIMARY KEY (produto,fornecedor),
+	FOREIGN KEY (produto) 
+		REFERENCES produtos(cod_barras)
+		ON DELETE CASCADE
+        ON UPDATE CASCADE,
+	FOREIGN KEY (fornecedor) 
+		REFERENCES fornecedores(id)
+		ON DELETE CASCADE
+        ON UPDATE CASCADE
 );
 
-CREATE TABLE IF NOT EXISTS produto (
-  cod_barras INT UNSIGNED AUTO_INCREMENT NOT NULL,
-  descricao VARCHAR(50) NOT NULL,
-  custo FLOAT(6, 2),
-  preco FLOAT(6, 2) NOT NULL,
-  fornecedor INT,
-  categoria INT,
-  PRIMARY KEY (cod_barras),
-  FOREIGN KEY (fornecedor) REFERENCES fornecedor(id),
-  FOREIGN KEY (categoria) REFERENCES categoria(id),
-  CHECK (preco > custo)
+CREATE TABLE IF NOT EXISTS metodo_pagamentos (
+	id INT UNSIGNED NOT NULL AUTO_INCREMENT, 
+	nome VARCHAR(40) UNIQUE,
+	PRIMARY KEY (id)
 );
 
-CREATE TABLE IF NOT EXISTS venda (
-  id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-  produtos JSON,
-  total FLOAT(6, 2),
-  desconto FLOAT(6, 2),
-  cliente INT UNSIGNED,
-  date DATETIME,
-  metodo_pagamento INT UNSIGNED,
-  PRIMARY KEY (id),
-  FOREIGN KEY (cliente) REFERENCES cliente(id),
-  FOREIGN KEY (metodo_pagamento) REFERENCES metodo_pagamento(id),
-  CHECK (total > 0)
+
+CREATE TABLE IF NOT EXISTS clientes (
+	id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+	nome VARCHAR(40) NOT NULL,
+	cpf VARCHAR(11) UNIQUE NOT NULL,
+	endereco VARCHAR(30) NOT NULL,
+	email VARCHAR(40),
+	telefone VARCHAR(11),
+	receber_oferta BOOLEAN,
+	PRIMARY KEY (id)
 );
 
-CREATE TABLE IF NOT EXISTS descontos (
-  id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-  porcentagem FLOAT(5, 2),
-  quantidade INT UNSIGNED,
-  formula VARCHAR(40),
-  PRIMARY KEY (id)
+CREATE TABLE IF NOT EXISTS turnos (
+	id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+	nome VARCHAR(30),
+	inicio TIME,
+	fim TIME,
+	PRIMARY KEY (id)
 );
+
 
 CREATE TABLE IF NOT EXISTS funcionarios (
-  cpf INT UNSIGNED NOT NULL AUTO_INCREMENT,
-  nome VARCHAR(40),
-  gerente SMALLINT,
-  usuario VARCHAR(40),
-  senha VARCHAR(40),
-  PRIMARY KEY (cpf)
+	id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+	cpf VARCHAR (11) UNIQUE NOT NULL,
+	nome VARCHAR(40) NOT NULL,
+	CNH VARCHAR (11) UNIQUE,
+    turno INT UNSIGNED,
+	usuario VARCHAR(40) UNIQUE,
+	senha VARCHAR(40),
+	PRIMARY KEY (id),
+    FOREIGN KEY (turno) 
+		REFERENCES turnos(id)
+        ON DELETE SET NULL
+        ON UPDATE CASCADE
 );
 
-CREATE TABLE IF NOT EXISTS turno (
-  id INT NOT NULL AUTO_INCREMENT,
-  nome VARCHAR(30),
-  inicio TIME,
-  fim TIME,
-  PRIMARY KEY (id)
+CREATE TABLE IF NOT EXISTS veiculos (
+	id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+	CRV VARCHAR(11) NOT NULL UNIQUE,
+	placa VARCHAR(7) NOT NULL UNIQUE,
+	modelo VARCHAR(40) NOT NULL,
+	cor VARCHAR(15) NOT NULL,
+	PRIMARY KEY (id)
 );
 
-CREATE VIEW venda_detalhes AS
-SELECT v.id AS venda_id, v.date, p.descricao AS produto, vd.quantidade, c.nome AS cliente
-FROM venda v
-JOIN produto p ON JSON_CONTAINS(v.produtos, CAST(CONCAT('{"produto_id": ', p.cod_barras, '}') AS JSON))
-JOIN venda_detalhes_aux vd ON v.id = vd.venda_id
-LEFT JOIN cliente c ON v.cliente = c.id;
 
-INSERT INTO categoria (nome) VALUES
-  ('Alimentos'),
-  ('Bebidas'),
-  ('Higiene'),
-  ('Limpeza'),
-  ('Eletrônicos');
+CREATE TABLE IF NOT EXISTS vendas (
+	nota_fiscal INT UNSIGNED NOT NULL,
+	cliente INT UNSIGNED,
+	data DATETIME NOT NULL,
+    vendedor  INT UNSIGNED NOT NULL,
+    entregador VARCHAR (11),
+    veiculo INT UNSIGNED,
+	PRIMARY KEY (nota_fiscal),
+	FOREIGN KEY (cliente) REFERENCES clientes(id),
+    FOREIGN KEY (vendedor) REFERENCES funcionarios(id),
+    FOREIGN KEY (entregador) REFERENCES funcionarios(CNH),
+    FOREIGN KEY (veiculo) REFERENCES veiculos(id),
+    check( (veiculo is null and entregador is null) or (veiculo is not null and entregador is not null))
+);
 
-INSERT INTO metodo_pagamento (descricao) VALUES
-  ('Dinheiro'),
-  ('Cartão de Crédito'),
-  ('Cartão de Débito'),
-  ('Pix'),
-  ('Transferência Bancária');
+CREATE TABLE IF NOT EXISTS pagamentos (
+	metodo_pagamento INT UNSIGNED NOT NULL, 
+	venda  INT UNSIGNED NOT NULL,
+	valor FLOAT(6, 2),
+	PRIMARY KEY (metodo_pagamento,venda),
+	FOREIGN KEY (metodo_pagamento) 
+		REFERENCES metodo_pagamentos(id)
+		ON UPDATE CASCADE,
+	FOREIGN KEY (venda) 
+		REFERENCES vendas(nota_fiscal)
+		ON UPDATE CASCADE,
+    check(valor > 0)
+);
 
-INSERT INTO fornecedor (nome, telefone, email) VALUES
-  ('Fornecedor A', 1234567890, 'fornecedorA@example.com'),
-  ('Fornecedor B', 9876543210, 'fornecedorB@example.com'),
-  ('Vendedor C', 5555555555, 'vendedorC@example.com');
 
-INSERT INTO produto (descricao, custo, preco, fornecedor, categoria) VALUES
-  ('Arroz', 2.50, 5.00, 1, 1),
-  ('Refrigerante', 1.00, 2.50, 2, 2),
-  ('Sabonete', 0.50, 1.20, 1, 3),
-  ('TV LED', 300.00, 450.00, 3, 5),
-  ('Laptop', 600.00, 850.00, 3, 5),
-  ('Feijão', 3.00, 6.50, 1, 1),
-  ('Água Mineral', 0.80, 1.50, 2, 2),
-  ('Shampoo', 1.50, 3.00, 1, 3),
-  ('Smartphone', 200.00, 300.00, 3, 5),
-  ('Mouse', 10.00, 15.00, 3, 5);
+CREATE TABLE IF NOT EXISTS carrinhos(
+	venda INT UNSIGNED NOT NULL,
+	produto INT UNSIGNED NOT NULL,
+	quantidade INT UNSIGNED NOT NULL,
+	PRIMARY KEY (produto,venda),
+	FOREIGN KEY (produto) REFERENCES produtos(cod_barras),
+	FOREIGN KEY (venda) 
+		REFERENCES vendas(nota_fiscal)
+		ON UPDATE CASCADE,
+    check(quantidade > 0)
+);
 
-INSERT INTO descontos (porcentagem, quantidade, formula) VALUES
-  (10.00, 100, 'Desconto de 10% em compras acima de R$100'),
-  (25.00, 3, '25% de desconto na compra de 3 produtos ou mais'),
-  (100.00, 5, 'Leve 5 produtos e pague somente 4');
 
-INSERT INTO cliente (nome, cpf, endereco, telefone, email) VALUES
-  ('João da Silva', '12345678901', 'Rua A, 123', '987654321', 'joao@example.com'),
-  ('Maria Oliveira', '98765432101', 'Avenida B, 456', '123456789', 'maria@example.com');
 
-INSERT INTO turno (nome, inicio, fim) VALUES
-  ('Manhã', '08:00:00', '12:00:00'),
-  ('Tarde', '13:00:00', '18:00:00'),
-  ('Noite', '18:00:00', '22:00:00');
+-- Nro nota fiscal, valor total, subtotal, descontos, ...  Vendas -> Carrinho -> Produtos]
+CREATE VIEW resumo_carrinhos AS
+SELECT venda as nota_fiscal,cod_barras,descricao,quantidade,preco, (preco * quantidade) as valor_total_bruto,
+		(preco * quantidade * desconto/100) as desconto,
+		((preco * quantidade) - (preco * quantidade * desconto/100)) as valor_final
+FROM carrinhos car 
+JOIN produtos pro ON (car.produto = pro.cod_barras);
 
-INSERT INTO funcionarios (nome, cpf, gerente, usuario, senha) VALUES
-  ('Pedro Souza', '55555555555', 1, 'pedro', 'senha123'),
-  ('Ana Santos', '66666666666', 0, 'ana', 'senha456');
+INSERT INTO categorias (nome) VALUES
+('Alimentos'),
+('Bebidas'),
+('Higiene'),
+('Limpeza'),
+('Eletrônicos');
 
-INSERT INTO venda (produtos, total, desconto, cliente, date, metodo_pagamento) VALUES
-  ('[{"produto_id": 1, "quantidade": 2}, {"produto_id": 2, "quantidade": 3}]', 15.50, 2.00, 1, NOW(), 2),
-  ('[{"produto_id": 4, "quantidade": 1}, {"produto_id": 5, "quantidade": 1}]', 130.00, 10.00, NULL, NOW(), 1);
+INSERT INTO metodo_pagamentos (nome) VALUES
+('Dinheiro'),
+('Cartão de Crédito'),
+('Cartão de Débito'),
+('Pix'),
+('Transferência Bancária'),
+('Vale alimentação');
+
+INSERT INTO fornecedores (nome, telefone, email, cpf_cnpj) VALUES
+('Fornecedor A', '1234567890', 'fornecedorA@example.com','10301031010'),
+('Fornecedor B', '9876543210', 'fornecedorB@example.com','0130120310013'),
+('Vendedor C', '5555555555', 'vendedorC@example.com','102303010320'),
+('Vendedor D', '5555555554', 'vendedorD@example.com','102344410320');
+
+INSERT INTO produtos (descricao, custo, preco, desconto, categoria) VALUES
+('Arroz', 2.50, 5.00, 10.00, 1),
+('Refrigerante', 1.00, 2.50, 0, 2),
+('Sabonete', 0.50, 1.20, 10.00, 3),
+('TV LED', 300.00, 450.00, 30.00, 5),
+('Laptop', 600.00, 850.00, 0, 5),
+('Feijão', 3.00, 6.50, 0, 1),
+('Água Mineral', 0.80, 1.50, 0, 2),
+('Shampoo', 1.50, 3.00, 5, 3),
+('Smartphone', 200.00, 300.00, 0, 5),
+('Mouse', 10.00, 15.00, 0, 5);
+
+INSERT INTO clientes (nome, cpf, endereco, telefone, email) VALUES
+('João da Silva', '12345678901', 'Rua A, 123', '987654321', 'joao@example.com'),
+('Maria Oliveira', '98765432101', 'Avenida B, 456', '123456789', 'maria@example.com');
+
+INSERT INTO turnos (nome, inicio, fim) VALUES
+('Integral','08:00:00', '18:00:00'),
+('Manhã', '08:00:00', '12:00:00'),
+('Tarde', '13:00:00', '18:00:00'),
+('Noite', '18:00:00', '22:00:00');
+
+
+INSERT INTO funcionarios (nome, cpf, usuario, senha,CNH, turno) VALUES
+('Pedro Souza', '55555555555','pedro',  'senha123','12345678910',1),
+('Marcelo Pereira', '11111111111','jorge',  'senha123','12342923910',4),
+('Ana Santos', '66666666666',  null, null,null,3),
+('Jorge Kritz', '22222222222',  null, null,null,2);
+
+INSERT INTO fornecimentos(fornecedor,produto) VALUES
+(1,1),
+(1,2),
+(1,3),
+(2,4),
+(2,5),
+(2,6),
+(3,6),
+(3,7),
+(3,8),
+(3,9),
+(3,10),
+(4,1),
+(4,2),
+(4,3);
+
+INSERT INTO veiculos(CRV ,placa, modelo, cor) VALUES
+('12345678910','ASB23JN','Fiorino 2005','Vermelho'),
+('12345678920','ALB23JN','Kombi 1995','Branco'),
+('12345678980','ABB23JN','Parati 2008','Azul');
+
+INSERT INTO vendas (nota_fiscal, cliente, data, vendedor, entregador, veiculo) VALUES
+(0, 1, NOW(), 3,'12345678910', 2),
+(1, 2, NOW(), 3,'12342923910', 1),
+(2, null, NOW(),4, null , null);
+
+INSERT INTO carrinhos(venda,produto,quantidade) VALUES
+(0,2,3),
+(0,1,2),
+(0,3,1),
+(0,9,1),
+(0,10,2),
+(0,7,3),
+(1,2,2),
+(1,1,1),
+(1,9,1),
+(2,10,1),
+(2,7,2),
+(2,2,3),
+(2,1,5),
+(2,9,1);
+-- select nota_fiscal, SUM(valor_final) from resumo_carrinhos group by nota_fiscal order by nota_fiscal 
+INSERT INTO pagamentos(metodo_pagamento, venda , valor) VALUES
+(2,0,352.08),
+(1,1,200),
+(4,1,109.5),
+(1,2,200),
+(2,2,148);
